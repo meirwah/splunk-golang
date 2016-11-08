b6c10ba8a4884eeeccf1ba27c22ec53c4d2d95c2
Amichay Spivak <amichay@demisto.com>
2016-11-08 11:41:08 +0200

    Update http.go

--- http.go
+++ http.go
@@ -12,6 +12,13 @@ import (
 /*
  * HTTP helper methods
  */
+func httpClient() *http.Client {
+         tr := &http.Transport{
+                 TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+         }
+         client := &http.Client{Transport: tr}
+         return client
+ }
 
 func (conn SplunkConnection) httpGet(url string, data *url.Values) (string, error) {
 	if response, err := conn.httpCall(url,"GET",data);err != nil {
@@ -41,7 +48,11 @@ func (conn SplunkConnection) httpCall(url string, method string, data *url.Value
 
         request, err := http.NewRequest(method, url, payload)
         conn.addAuthHeader(request)
-        response, err := conn.HttpClient.Do(request)
+	client := conn.HttpClient
+	if client == nil {
+		client = httpClient()
+	}
+        response, err := client.Do(request)
 
         if err != nil {
                 return nil, err
